7e29e2b88e631e44e213bc53bea14f8235a0b2b9
gwanii <lung4536@gmail.com>
2023-03-18 16:17:40 +0900

    first commit

---
+++ src/main/java/info/querypro/querypro/QueryproApplication.java
@@ -0,0 +1,13 @@
+package info.querypro.querypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryproApplication.class, args);
+    }
+
+}
